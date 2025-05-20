1dddc34863b11dd8c788b5b13cfbf354bc432899
Akhilan-J <zatorakhilan@gmail.com>
2025-05-20 17:53:00 -0400

    Added dark mode

---
+++ Home.js
@@ -0,0 +1,31 @@
+const now = new Date();
+const Time = now.toLocaleTimeString();
+const CurrTime = parseInt(Time.slice(0, 2));
+let greet = "";
+if (CurrTime >= 0 && CurrTime < 12) {
+  greet = "Morning";
+} else {
+  greet = "Evening";
+}
+const greeting = document.getElementById("Greeting");
+greeting.innerText = `Good ${greet} user`;
+
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
